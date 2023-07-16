5223005838906ca58e9497f5d37583ff2ceda1e6
Mike John <eviotamikejohnb@gmail.com>
2023-07-16 15:12:59 +0800

    g

--- src/lib/GenComp/BaniInputs.svelte
+++ src/lib/GenComp/BaniInputs.svelte
@@ -5,17 +5,13 @@
     export let type = "text";
     export let placeholder = "Placehold";
 
-    //some bullsht estetik
-    const cssObj = {animate: "animate-none"}
-    const onFocus = () => cssObj.animate = "animate-bounce";
-    const outFocus = () => cssObj.animate = "animate-none";
     
 </script>
 <!--@component this component takes label, bindthis(as data binder), type, placeholder-->
 {#if type === "text"}
     <main>
         <p class="px-2 font-semibold font-mono">{label}</p>
-        <input type="text" {placeholder} class="w-full px-2  py-1 focus:outline-none border-[0.1rem] border-black rounded-sm {cssObj.animate} " on:focusin={onFocus} on:focusout={outFocus} 
+        <input type="text" {placeholder} class="w-full px-2  py-1 focus:outline-none border-[0.1rem] border-black rounded-sm" 
         bind:value={bindthis}
         />
     </main>
@@ -23,7 +19,7 @@
 {:else if type === "email"}
     <main>
         <p class="px-2 font-semibold font-mono">{label}</p>
-        <input type="email" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm {cssObj.animate} " on:focusin={onFocus} on:focusout={outFocus} 
+        <input type="email" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm" 
         bind:value={bindthis}
         />
     </main>
@@ -31,7 +27,7 @@
 {:else if type === "password"}
     <main>
         <p class="px-2 font-semibold font-mono">{label}</p>
-        <input type="password" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm {cssObj.animate} " on:focusin={onFocus} on:focusout={outFocus} 
+        <input type="password" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm" 
         bind:value={bindthis}
         />
     </main>
@@ -39,7 +35,7 @@
 {:else if type === "number"}
     <main>
         <p class="px-2 font-semibold font-mono">{label}</p>
-        <input type="number" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm {cssObj.animate} " on:focusin={onFocus} on:focusout={outFocus} 
+        <input type="number" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm" 
         bind:value={bindthis}
         />
     </main>
@@ -47,7 +43,7 @@
 {:else if type === "date"}
     <main>
         <p class="px-2 font-semibold font-mono">{label}</p>
-        <input type="date" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm {cssObj.animate} " on:focusin={onFocus} on:focusout={outFocus} 
+        <input type="date" {placeholder} class="w-full px-2 py-1 focus:outline-none border-[0.1rem] border-black rounded-sm" 
         bind:value={bindthis}
         />
     </main>
